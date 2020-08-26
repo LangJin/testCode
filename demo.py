0861901f8516e0ec75fa9fb8e6bfc414d2dd4193
langjin <fenyukuang@163.com>
2020-08-26 22:32:43 +0800

    第一次python课程的代码

--- demo.py
+++ demo.py
@@ -1,1 +1,30 @@
-print("你好，浪晋的测试小讲堂！")
+"""
+print("你好，浪晋的测试小讲堂！",end="1111111")
+print(2333)
+print(2.333)
+print(True,False)
+print(None)
+print(())
+print([])
+print({})
+print("哈哈",2333,"你好")
+
+
+name = "张三"
+print(name)
+
+res = (1+2+4+5*2/(4*2))%3
+print(res)
+
+a = input("请输入：")
+print("这是我刚刚输入的值：",a)
+
+# 哈哈哈 今天你吃了嘛
+# 单行注释的快捷键 ctrl + / 
+
+a = int("2333")
+print(type(a))
+"""
+
+a = len("哈哈哈哈哈哈哈哈哈哈哈哈哈")
+print(a)
